9aa7feadd2462b96204cd3270b2217aaaee7cbb9
tsalla <tilak.04@yahoo.com>
2022-02-21 13:57:12 +0530

    Updating ansible

--- create-ec2.sh
+++ create-ec2.sh
@@ -56,8 +56,7 @@ INSTANCE_CREATE() {
 ### Main Program
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
-
-ami-07ac8fcd4c7338744
+AMI_ID=ami-07ac8fcd4c7338744
 
 if [ "$1" == "list" ]; then
   aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
